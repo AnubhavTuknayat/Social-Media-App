53d2d768f2f0dc5cc88c2fcbb0a0f7fe4da5dad4
Anubhav Tuknayat <anubhavtuknayat500@gmail.com>
2024-03-18 00:40:16 +0530

    added gitignore

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import {fileURLToPath} from "url";
+import {register} from "./contollers/auth.js"
 
 // CONFIGS:
 const filename = fileURLToPath(import.meta.url)
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+// ROUTES
+app.post("/auth/register",upload.single("picture"),register);
 
 // MONGOOSE SETUP:
 const PORT = process.env.PORT || 8000;
