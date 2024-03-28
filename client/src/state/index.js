8aaec4547611e9ac14629973c8c9637a490c4541
Anubhav Tuknayat <anubhavtuknayat500@gmail.com>
2024-03-29 00:14:49 +0530

    typo

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -48,4 +48,4 @@ export const authSlice = createSlice ({
 })
 
 export const {setMode,setLogin,setLogout,setFriends,setPosts,setPost} = authSlice.actions;
-export default authSlice.reducers;
+export default authSlice.reducer;
