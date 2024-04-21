7dcab61d3ecdeddded2a672b8530a3cfe9770862
Anubhav Tuknayat <anubhavtuknayat500@gmail.com>
2024-04-21 16:19:31 +0530

    v

--- server/index.js
+++ server/index.js
@@ -30,7 +30,13 @@ app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-social-media.vercel.app"],
+        methods: ["POST","GET"],
+        credentials:true
+    }
+));
 app.use("/assets",express.static(path.join(dirname,'public/assets')));
 
 //FILE STORAGE:
